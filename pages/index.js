b8b5c44d026ed83c227a97eefdde3515bf14c26a
Torben Jost <torbenjost@proton.me>
2024-10-17 11:58:54 +0200

    implement API search logic

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,33 @@
+import React from "react";
+import useSWR from "swr";
+import InputComp from "@/components/SearchInput/SearchInput";
+import { useState } from "react";
+import styled from "styled-components";
+
 export default function HomePage() {
+  const [keyWords, setKeywords] = useState("blanchett");
+
+  // Data fetching
+  const [url, setUrl] = useState(null);
+  const fetcher = (url) => fetch(url).then((res) => res.json());
+  const { data, error } = useSWR(url, fetcher, { shouldRetryOnError: false });
+  const isLoading = !error && !data && !!url;
+
+  // Keyword Search
+  const handleSearch = () => {
+    setUrl(
+      `https://api.netzkino.de.simplecache.net/capi-2.0a/search?q=${keyWords}&d=devtest`
+    );
+    console.log("url", url);
+  };
+
   return (
     <div>
-      <h1>Hello from Next.js</h1>
+      <InputComp
+        id="keywords"
+        value={keyWords}
+        onChange={(e) => setKeywords(e.target.value)}
+      />
     </div>
   );
 }
